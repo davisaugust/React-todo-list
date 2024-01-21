6e125100aad103eb64504dcf5c3db37225f0afd4
davisaugust <davisaugust004@gmail.com>
2024-01-21 17:25:54 -0300

    Fix: Fixing and adjusting some details.

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,15 +4,17 @@ import React from "react";
 const TodoForm = ( {addTodo} ) => {
   const [value, setValue] = useState("");
   const [category, setCategory] = useState("");
+  const [priority, setPriority] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!value || !category) return;
+    if (!value || !category || !priority) return;
     // adicionar todo
-    addTodo(value, category);
+    addTodo(value, category, priority);
     // limpar os campos
     setValue("");
     setCategory("");
+    setPriority("");
   };
 
   return (
@@ -36,7 +38,20 @@ const TodoForm = ( {addTodo} ) => {
           <option value="Pessoal">Pessoal</option>
           <option value="Estudos">Estudos</option>
         </select>
-        <button type="submit">Criar tarefa</button>
+
+        <select
+          value={priority}
+          name=""
+          id=""
+          onChange={(e) => setPriority(e.target.value)}
+        >
+          <option value="">Selecione o nível de prioridade</option>
+          <option value="urgency">Urgente</option>
+          <option value="noturgent">Não urgente</option>
+          <option value="Tranquilo">Tranquilo</option>
+        </select>
+        <button type=
+        "submit">Criar tarefa</button>
       </form>
     </div>
   );
